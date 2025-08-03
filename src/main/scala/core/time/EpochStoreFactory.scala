39ecdb3c633194441bc2cee2673570ea806088f1
abh80 <abhb787@outlook.com>
2025-08-03 17:12:50 +0530

    refactor: added type annotation for public members

--- src/main/scala/core/time/EpochStoreFactory.scala
+++ src/main/scala/core/time/EpochStoreFactory.scala
@@ -13,25 +13,25 @@ object EpochStoreFactory {
   /** The J2000.0 epoch, which is the fundamental epoch for the ICRF reference frame.
    * Defined as January 1, 2000, at 12:00 TT (Terrestrial Time).
    */
-  val J2000_0 = Date(2000, 1, 1)
+  val J2000_0: Date = Date(2000, 1, 1)
 
   /** The Julian epoch, which marks the beginning of the Julian calendar.
    * Defined as January 1, 4713 BCE at 12:00 UT.
    */
-  val JULIAN = Date(-4713, 1, 1)
+  val JULIAN: Date = Date(-4713, 1, 1)
 
   /** The Unix epoch, used as the starting point for Unix timestamps.
    * Defined as January 1, 1970, at 00:00:00 UTC.
    */
-  val UNIX = Date(1970, 1, 1)
+  val UNIX: Date = Date(1970, 1, 1)
 
   /** The CXCSEC epoch, used by the Chandra X-ray Observatory.
    * Defined as January 1, 1998, at 00:00:00 TT.
    */
-  val CXCSEC = Date(1998, 1, 1)
+  val CXCSEC: Date = Date(1998, 1, 1)
 
   /** The GPS epoch, used by the Global Positioning System.
    * Defined as January 6, 1980, at 00:00:00 UTC.
    */
-  val GPS = Date(1980, 1, 6)
+  val GPS: Date = Date(1980, 1, 6)
 }
