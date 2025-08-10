e850da92e519768a7a4dcf8e54bc6509518fc2ff
abh80 <abhb787@outlook.com>
2025-08-10 16:26:56 +0530

    refactor: remove hardcoded simplified values to increase precision

--- src/main/scala/core/time/TDBScale.scala
+++ src/main/scala/core/time/TDBScale.scala
@@ -2,9 +2,10 @@ package org.abh80.nf
 package core.time
 
 import breeze.numerics.*
+import org.abh80.nf.core.metrics.AngleUnit
 
-private val G0 = 6.24004077 // 357.528 degrees in radians
-private val G1 = 0.01720197 // 0.9856003
+private val G0 = AngleUnit.Degree(357.53).toRadians
+private val G1 = AngleUnit.Degree(0.9856003).toRadians
 
 private val F1 = 0.001658
 private val F2 = 0.000028
@@ -17,7 +18,7 @@ class TDBScale(val tt: TimeScale, j2000Epoch: AbsoluteTime = EpochFactory.J2000_
   override def timePastTAI(date: AbsoluteTime): TimeFormat =
     val jdtt = date.durationFrom(j2000Epoch).toDouble / 86400.0
 
-    val g = G0 + G1 * (jdtt - 2451545.0)
+    val g = G0 + G1 * jdtt
 
-    tt.timePastTAI(date) + TimeFormat.fromDouble(F1 * sin(g) + F2 * sin(2 * g))
+    tt.timePastTAI(date) + TimeFormat.fromDouble(sin(g) * (F1 + F2 * cos(g)))
 }
