1d809c985397084be48ec5080ec4b9e144a415af
abh80 <abhb787@outlook.com>
2025-08-19 20:10:23 +0530

    refactor: align epoch definitions with AbsoluteTime for consistency and clarity

--- src/main/scala/core/time/EpochFactory.scala
+++ src/main/scala/core/time/EpochFactory.scala
@@ -13,27 +13,27 @@ object EpochFactory {
   /** The J2000.0 epoch, which is the fundamental epoch for the ICRF reference frame.
    * Defined as January 1, 2000, at 12:00 TT (Terrestrial Time).
    */
-  val J2000_0: AbsoluteTime = AbsoluteTime(Date(2000, 1, 1), Time(12, 0, TimeFormat.Zero), TimeScaleFactory.getTT)
+  val J2000_0: AbsoluteTime = AbsoluteTime(Date.J2000_0, Time(12, 0, TimeFormat.Zero), TimeScaleFactory.getTT)
 
-    /** The J1900 epoch, Defined as January 1, 1900, at 12:00 TT. A century before [[EpochFactory.J2000_0]].
+  /** The J1900 epoch, Defined as January 1, 1900, at 12:00 TT. A century before [[EpochFactory.J2000_0]].
    * This is useful to implement leap seconds for [[UTCScale]], since leap seconds started the year ago 2000.
-   *  See: https://naif.jpl.nasa.gov/pub/naif/toolkit_docs/FORTRAN/spicelib/j1900.html */
+   * See: https://naif.jpl.nasa.gov/pub/naif/toolkit_docs/FORTRAN/spicelib/j1900.html */
   val J1900: AbsoluteTime = AbsoluteTime(Date(1900, 1, 1), Time(12, 0, TimeFormat.Zero), TimeScaleFactory.getTT)
 
   /** The Julian epoch, which marks the beginning of the Julian calendar.
    * Defined as January 1, 4713, BCE at 12:00 UT.
    */
-  val JULIAN: Date = Date(-4713, 1, 1)
+  val JULIAN: AbsoluteTime = AbsoluteTime(Date.JULIAN, Time(12, 0 , TimeFormat.Zero), TimeScaleFactory.getTT)
   /** The Unix epoch, used as the starting point for Unix timestamps.
    * Defined as January 1, 1970, at 00:00:00 UTC.
    */
-  val UNIX: Date = Date(1970, 1, 1)
+  val UNIX: AbsoluteTime = AbsoluteTime(Date.UNIX, Time(0, 0, TimeFormat.Zero), TimeScaleFactory.getUTC)
   /** The CXCSEC epoch, used by the Chandra X-ray Observatory.
    * Defined as January 1, 1998, at 00:00:00 TT.
    */
-  val CXCSEC: Date = Date(1998, 1, 1)
+  val CXCSEC: AbsoluteTime = AbsoluteTime(Date.CXCSEC, Time(0, 0, TimeFormat.Zero), TimeScaleFactory.getTT)
   /** The GPS epoch, used by the Global Positioning System.
    * Defined as January 6, 1980, at 00:00:00 UTC.
    */
-  val GPS: Date = Date(1980, 1, 6)
+  val GPS: AbsoluteTime = AbsoluteTime(Date.GPS, Time(0, 0, TimeFormat.Zero), TimeScaleFactory.getUTC)
 }
