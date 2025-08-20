ab8df5ae9ee513610374ecfc95455fa611f673dd
abh80 <abhb787@outlook.com>
2025-08-21 01:00:13 +0530

    fix: correct typo in GPS time scale drift explanation in test

--- src/test/scala/core/time/GPSScaleSpec.scala
+++ src/test/scala/core/time/GPSScaleSpec.scala
@@ -15,7 +15,7 @@ class GPSScaleSpec extends AnyFunSuite with Matchers {
 
   test("utc drift from gps") {
     /* Over time the UTC will appear slower than GPS, since GPS is fixedly offset from UTC at Midnight 1980, January, 6. */
-    /* For this case, by 2017 a total of 37 leap seconds have been added, so UTC = TAI - 37 and GPS = TAI - 19. Therefore, GPS = UTC + 18 an 2017. */
+    /* For this case, by 2017 a total of 37 leap seconds have been added, so UTC = TAI - 37 and GPS = TAI - 19. Therefore, GPS = UTC + 18 in 2017. */
     val ut = AbsoluteTime(2017, 12, 31, 23, 59, 59, TimeScaleFactory.getUTC)
     val gt = AbsoluteTime(2018, 1, 1, 0, 0, 17, GPS)
 
